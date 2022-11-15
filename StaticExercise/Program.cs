4f7beed85c390a3a47267a46b18657f43e8a8a03
benjamin kunkel <benjamink1125@gmail.com>
2022-11-14 16:46:18 -0800

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿public static class TempConvertor
+
+    
+{
+   var celsius = TempConverter.FahrenheightToCelsius(68);
+
+    var fahrenheight = TempConverter.CelsiusToFahrenheight(20);
+
+        Console.WriteLine($"celsius {celsius}");
+
+        Console.WriteLine($"fahrenheight {fahrenheight}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace StaticExercise
+{
+    public class TempConverter
+    {
+        using System;
+namespace StaticExercise
+    {
+        public class TempConverter
+        {
+
+            public static class TempConvertor
+            {
+                public static double FahrenheitToCelsius(double fahrenheit)
+                {
+                    return (fahrenheit - 32) / 1.8;
+
+                    var result;
+
+                }
+
+                public static double CelsiusToFahrenheit(double celsius)
+                {
+                    return (celsius * 9) / 5 + 32;
+                }
+            }
+        {
+        }
+    }
+}
+
